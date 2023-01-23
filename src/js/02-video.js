3730caf1b2c94433bf2a28264723b3225733cf0a
KhailoAndrey <hap512@gmail.com>
2023-01-24 00:54:01 +0200

    task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import player from '@vimeo/player';
+import _ from 'lodash';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo.Player(iframe);
+
+const onPlay = function (timeupdate) {
+  _.throttle(() => {
+    localStorage.setItem('videoplayer-current-time', timeupdate.seconds), 1000;
+  });
+};
+
+player.on('play', onPlay);
+
+if (localStorage.getItem('videoplayer-current-time') || 0) {
+  player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+}
