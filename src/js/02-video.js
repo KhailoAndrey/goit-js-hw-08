4a96923ff51c78baf79269fbc35fe5370a55bcd9
KhailoAndrey <hap512@gmail.com>
2023-01-24 01:25:14 +0200

    task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,17 +1,17 @@
-import player from '@vimeo/player';
-import _ from 'lodash';
+import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('#vimeo-player');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
-const onPlay = function (timeupdate) {
-  _.throttle(() => {
-    localStorage.setItem('videoplayer-current-time', timeupdate.seconds), 1000;
-  });
+const onPlay = function (data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+  console.log(data.seconds);
 };
 
-player.on('play', onPlay);
+const throttled = throttle(onPlay, 1000);
+
+player.on('timeupdate', throttled);
 
 if (localStorage.getItem('videoplayer-current-time') || 0) {
   player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
